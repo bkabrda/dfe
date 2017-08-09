11ba16d7536f78d618badb139eaf6a419077aca7
Slavek Kabrda <bkabrda@redhat.com>
2017-08-09 09:12:32 +0200

    Remove unused commandline argument

--- dfe/bin.py
+++ dfe/bin.py
@@ -6,7 +6,6 @@ from dfe.renderer import Renderer
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--configurations', default='configurations.yml')
-    parser.add_argument('-d', '--dockerfile', default='Dockerfile.template')
     meg = parser.add_mutually_exclusive_group(required=True)
     meg.add_argument('-l', '--list-configurations', action='store_true')
     meg.add_argument('-e', '--config-as-env-vars')
