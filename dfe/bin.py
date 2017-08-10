147d205a8962cedcf3d50e55dae93e6cbb0dc7af
Slavek Kabrda <bkabrda@redhat.com>
2017-08-10 11:59:39 +0200

    Add help. Fixes #4

--- dfe/bin.py
+++ dfe/bin.py
@@ -5,18 +5,48 @@ from dfe.renderer import Renderer
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--configurations', default='configurations.yml')
-    parser.add_argument('-i', '--input-dir', default='.')
-    parser.add_argument('-o', '--output-dir', default='.')
+    parser.add_argument(
+        '-c', '--configurations',
+        default='configurations.yml',
+        help='Path to configurations file, defaults to ./configurations.yml'
+    )
+    parser.add_argument(
+        '-i', '--input-dir',
+        default='.',
+        help='Path to directory with templates, defaults to "."'
+    )
+    parser.add_argument(
+        '-o', '--output-dir',
+        default='.',
+        help='Path to directory where to put rendered templates, defaults to "."'
+    )
     subp = parser.add_subparsers(dest='subparser')
-    subp.add_parser('list-configs')
-    val = subp.add_parser('config-value',
-                          help='Print given value from given config item, e.g. '
-                          '`fedora-26 vars.base_img_name` or `fedora-26 tag`')
-    val.add_argument('config')
-    val.add_argument('value')
-    rend = subp.add_parser('render')
-    rend.add_argument('config')
+    subp.add_parser(
+        'list-configs',
+        help='List names of all configurations items'
+    )
+    val = subp.add_parser(
+        'config-value',
+        help='Print given value from given config item, e.g. '
+        '`fedora-26 vars.base_img_name` or `fedora-26 tag`'
+    )
+    val.add_argument(
+        'config',
+        help='Name of configuration item to extract value from'
+    )
+    val.add_argument(
+        'value',
+        help='Name of item to extract value from. You can use dotted notation to access '
+        'sub-mappings, e.g. `vars.base_img_name`'
+    )
+    rend = subp.add_parser(
+        'render',
+        help='Render all files for given config'
+    )
+    rend.add_argument(
+        'config',
+        help='Config item to render files for'
+    )
     args = parser.parse_args()
     configs = Configurations.from_file(args.configurations, args.output_dir)
     if args.subparser == 'list-configs':
